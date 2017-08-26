1a4c1937af8915369ac4a41204a21a96d46b4bfc
Michael Brinkmeier <michael.brinkmeier@uni-osnabrueck.de>
2017-08-26 22:32:34 +0200

    * Added a timeout for compilation and upload requests (120000ms by default) * Fixed several problems with generated code and blocks * Introduced subclass Abbozza of MicroBit * Added int getGesture() to class Abbozza

--- lib/build/calliope/source/abbozza.h
+++ lib/build/calliope/source/abbozza.h
@@ -6,70 +6,12 @@
 #define _ABBOZZA_H
 
 #include "MicroBit.h"
+#include "abbozzaTools.h"
 
-MicroBit abbozza;
         
 /**
  * Define pin macros
  */
 
-#define TOUCH0  12
-#define TOUCH1  0
-#define TOUCH2  1
-#define TOUCH3  16
-
-#define C0      12
-#define C1      0
-#define C2      1
-#define C3      16
-#define C4      3
-#define C5      4
-#define C6      10
-#define C7      13
-#define C8      14
-#define C9      15
-#define C10     9
-#define C11     7
-#define C12     6
-#define C16     2
-#define C17     8
-#define C21     21
-
-
-/**
- * Images
- */
-
-const uint8_t __heart[] __attribute__ ((aligned (4))) = { 0xff,0xff,5,0,5,0, 0,255,0,255,0, 255,0,255,0,255, 255,0,0,0,255, 0,255,0,255,0, 0,0,255,0,0 };
-MicroBitImage Image_HEART((ImageData*) __heart);
-
-const uint8_t __small_heart[] __attribute__ ((aligned (4))) = { 0xff,0xff,5,0,5,0, 0,0,0,0,0, 0,255,0,255,0, 0,255,255,255,0, 0,0,255,0,0, 0,0,0,0,0 };
-MicroBitImage Image_HEART_SMALL((ImageData*) __small_heart);
-
-const uint8_t __happy[] __attribute__ ((aligned (4))) = { 0xff,0xff,5,0,5,0, 0,0,0,0,0, 0,255,0,255,0, 0,0,0,0,0, 255,0,0,0,255, 0,255,255,255,0 };
-MicroBitImage Image_HAPPY((ImageData*) __happy);
-
-const uint8_t __smiley[] __attribute__ ((aligned (4))) = { 0xff,0xff,5,0,5,0,  0,0,0,0,0, 0,255,0,255,0, 0,0,0,0,0, 0,255,255,255,0, 0,0,0,0,0 };
-MicroBitImage Image_SMILEY((ImageData*) __smiley);
-
-const uint8_t __sad[] __attribute__ ((aligned (4))) = { 0xff,0xff,5,0,5,0, 0,0,0,0,0, 0,255,0,255,0, 0,0,0,0,0, 0,255,255,255,0, 255,0,0,0,255 };
-MicroBitImage Image_SAD((ImageData*) __sad);
-
-const uint8_t __surprised[] __attribute__ ((aligned (4))) = { 0xff,0xff,5,0,5,0, 0,0,0,0,0, 0,255,0,255,0, 0,0,0,0,0, 0,0,255,0,0, 0,0,0,0,0 };
-MicroBitImage Image_SURPRISED((ImageData*) __surprised);
-
-const uint8_t __beard[] __attribute__ ((aligned (4))) = { 0xff,0xff,5,0,5,0, 0,0,0,0,0, 0,255,0,255,0, 0,0,0,0,0, 0,255,255,255,0, 0,255,0,255,0 };
-MicroBitImage Image_BEARD((ImageData*) __beard);
-
-const uint8_t __moustache[] __attribute__ ((aligned (4))) = { 0xff, 0xff, 5, 0, 5,0, 0,0,0,0,0, 0,255,0,255,0, 0,0,0,0,0, 255,255,0,255,255, 0,0,255,0,0 };
-MicroBitImage Image_MOUSTACHE((ImageData*) __moustache);
-
-const uint8_t __yes[] __attribute__ ((aligned (4))) = { 0xff, 0xff, 5, 0, 5,0, 0,0,0,0,0, 0,0,0,0,255, 0,0,0,255,0, 255,0,255,0,0, 0,255,0,0,0 };
-MicroBitImage Image_YES((ImageData*) __yes);
-
-const uint8_t __no[] __attribute__ ((aligned (4))) = { 0xff, 0xff, 5, 0, 5,0, 255,0,0,0,255, 0,255,0,255,0, 0,0,255,0,0, 0,255,0,255,0, 255,0,0,0,255 };
-MicroBitImage Image_NO((ImageData*) __no);
-
-uint8_t __abz_image_data[31] = { 0xff,0xff,5,0,5,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0};
 
 #endif
---
+++ lib/build/calliope/source/abbozzaTools.h
@@ -0,0 +1,62 @@
+/*
+ * This header file contains some utility function used by abbozza!
+ */
+ 
+#ifndef _ABBOZZA_TOOLS_H
+#define _ABBOZZA_TOOLS_H
+
+#include "MicroBit.h"
+
+/*
+ * Makros for the correctly numbered pins
+ */
+#define TOUCH0  12
+#define TOUCH1  0
+#define TOUCH2  1
+#define TOUCH3  16
+
+#define C0      12
+#define C1      0
+#define C2      1
+#define C3      16
+#define C4      3
+#define C5      4
+#define C6      10
+#define C7      13
+#define C8      14
+#define C9      15
+#define C10     9
+#define C11     7
+#define C12     6
+#define C16     2
+#define C17     8
+#define C21     21
+
+/*
+ * Images
+ */
+extern const uint8_t __heart[];
+extern const uint8_t __small_heart[];
+extern const uint8_t __happy[];
+extern const uint8_t __smiley[];
+extern const uint8_t __sad[];
+extern const uint8_t __surprised[];
+extern const uint8_t __beard[];
+extern const uint8_t __moustache[];
+extern const uint8_t __yes[];
+extern const uint8_t __no[];
+extern uint8_t __abz_image_data[31];
+
+/*
+ * Helper functions
+ */
+int abbozzaGetGesture(MicroBit abbozza);
+
+
+class Abbozza : public MicroBit {    
+    public:
+        int getGesture();
+};
+
+#endif
+
