c32af9fdddf879895d7348f6daf330f98e5cf914
Michael Brinkmeier <michael.brinkmeier@uni-osnabrueck.de>
2018-02-18 10:24:26 +0100

    * Added font size increase/decrease menu items

--- src/de/uos/inf/did/abbozza/calliope/AbbozzaCalliopeFrame.java
+++ src/de/uos/inf/did/abbozza/calliope/AbbozzaCalliopeFrame.java
@@ -115,7 +115,7 @@ public class AbbozzaCalliopeFrame  extends javax.swing.JFrame implements Abbozza
         sourceArea.setTabSize(3);
         supplier = new AbbozzaCalliopeTooltipSupplier();
         sourceArea.setToolTipSupplier(supplier);
-        Font font = new Font("Courier New",Font.PLAIN,14);
+        Font font = new Font("Cour<ier New",Font.PLAIN,14);
         if ( !font.getFontName().equals("Courier New") ) {
            font = new Font("DejaVu Sans Mono",Font.PLAIN,14); 
         }
@@ -246,6 +246,9 @@ public class AbbozzaCalliopeFrame  extends javax.swing.JFrame implements Abbozza
         cleanItem = new javax.swing.JMenuItem();
         jSeparator1 = new javax.swing.JPopupMenu.Separator();
         settingsItem = new javax.swing.JMenuItem();
+        jMenu1 = new javax.swing.JMenu();
+        incFontSizeItem = new javax.swing.JMenuItem();
+        decFontSizeItem = new javax.swing.JMenuItem();
 
         compileButton2.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
         compileButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/de/uos/inf/did/abbozza/calliope/icons/save.png"))); // NOI18N
@@ -340,11 +343,11 @@ public class AbbozzaCalliopeFrame  extends javax.swing.JFrame implements Abbozza
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 76, Short.MAX_VALUE)
+                .addGap(0, 78, Short.MAX_VALUE)
                 .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addComponent(messagePane, javax.swing.GroupLayout.DEFAULT_SIZE, 78, Short.MAX_VALUE)
+                    .addComponent(messagePane, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
                     .addContainerGap()))
         );
 
@@ -550,6 +553,28 @@ public class AbbozzaCalliopeFrame  extends javax.swing.JFrame implements Abbozza
 
         menuBar.add(abbozzaMenu);
 
+        jMenu1.setText(AbbozzaLocale.entry("gui.view"));
+
+        incFontSizeItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_PLUS, java.awt.event.InputEvent.CTRL_MASK));
+        incFontSizeItem.setText(AbbozzaLocale.entry("gui.inc_font_size"));
+        incFontSizeItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                incFontSizeItemActionPerformed(evt);
+            }
+        });
+        jMenu1.add(incFontSizeItem);
+
+        decFontSizeItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_MINUS, java.awt.event.InputEvent.CTRL_MASK));
+        decFontSizeItem.setText(AbbozzaLocale.entry("gui.dec_font_size"));
+        decFontSizeItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                decFontSizeItemActionPerformed(evt);
+            }
+        });
+        jMenu1.add(decFontSizeItem);
+
+        menuBar.add(jMenu1);
+
         setJMenuBar(menuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -562,7 +587,7 @@ public class AbbozzaCalliopeFrame  extends javax.swing.JFrame implements Abbozza
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(splitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 628, Short.MAX_VALUE)
+            .addComponent(splitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
         );
 
         pack();
@@ -737,6 +762,27 @@ public class AbbozzaCalliopeFrame  extends javax.swing.JFrame implements Abbozza
 
     }//GEN-LAST:event_cleanItemActionPerformed
 
+    private void incFontSizeItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_incFontSizeItemActionPerformed
+        Font oldfont = sourceArea.getFont();
+        int size = ( oldfont.getSize()+1 ) % 80;
+        Font newfont = new Font("Cour<ier New",Font.PLAIN,size);
+        if ( !newfont.getFontName().equals("Courier New") ) {
+           newfont = new Font("DejaVu Sans Mono",Font.PLAIN,size); 
+        }
+        sourceArea.setFont(newfont);
+    }//GEN-LAST:event_incFontSizeItemActionPerformed
+
+    private void decFontSizeItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_decFontSizeItemActionPerformed
+        Font oldfont = sourceArea.getFont();
+        int size = ( oldfont.getSize()-1 );
+        if ( size < 5 ) size = 5;
+        Font newfont = new Font("Cour<ier New",Font.PLAIN,size);
+        if ( !newfont.getFontName().equals("Courier New") ) {
+           newfont = new Font("DejaVu Sans Mono",Font.PLAIN,size); 
+        }
+        sourceArea.setFont(newfont);
+    }//GEN-LAST:event_decFontSizeItemActionPerformed
+
  
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu abbozzaMenu;
@@ -749,9 +795,12 @@ public class AbbozzaCalliopeFrame  extends javax.swing.JFrame implements Abbozza
     private javax.swing.JEditorPane consoleArea;
     private javax.swing.JScrollPane consolePane;
     private javax.swing.JRadioButtonMenuItem debugMenuItem;
+    private javax.swing.JMenuItem decFontSizeItem;
     private javax.swing.JPanel editorPane;
     private javax.swing.JRadioButtonMenuItem errMenuItem;
+    private javax.swing.JMenuItem incFontSizeItem;
     private javax.swing.JRadioButtonMenuItem infoMenuItem;
+    private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JPanel jPanel1;
