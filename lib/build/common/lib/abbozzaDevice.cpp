0c323c99cba593e70f0b615411d167b2216850a8
michael <michael@ridcully>
2018-06-16 16:13:01 +0200

    * Added i2c request

--- lib/build/common/lib/abbozzaDevice.cpp
+++ lib/build/common/lib/abbozzaDevice.cpp
@@ -387,7 +387,7 @@ ManagedString Abbozza::i2cRequest(uint8_t addr, int len) {
 
 ManagedString Abbozza::i2cRequest(uint8_t addr, uint8_t reg, int len) {
     i2cData[0] = reg;
-    i2c.write((addr*2), reg);
+    i2c.write((addr*2),i2cData,1);
     i2c.read((addr*2)+1,i2cData,len);
     ManagedString result(i2cData,len);
     return result;
