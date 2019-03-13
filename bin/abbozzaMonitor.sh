dc2588ae402541a91a2886afb490ca57761df88f
mbrin <mbrinkmeier@uni-osnabrueck.de>
2019-03-13 21:54:53 +0100

    * Serial communication changed to jSerialComm

--- bin/abbozzaMonitor.sh
+++ bin/abbozzaMonitor.sh
@@ -1,1 +1,1 @@
-java -cp ../lib/jssc-2.8.0.jar:../lib/abbozza-calliope.jar../lib/websockets.jar:../lib/slf4j-api-1.7.25.jar de.uos.inf.did.abbozza.monitor.AbbozzaMonitorServer
+java -cp ../lib/jSerialComm-2.4.2.jar:../lib/abbozza-calliope.jar../lib/websockets.jar:../lib/slf4j-api-1.7.25.jar de.uos.inf.did.abbozza.monitor.AbbozzaMonitorServer
