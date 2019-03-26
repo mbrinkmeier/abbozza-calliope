34cd750f7fa8da8fa86fb1d387db3dac666b5aee
Michael Brinkmeier <mbrinkmeier@uni-osnabrueck.de>
2019-03-26 20:54:31 +0100

    * java used to start

--- bin/abbozzaC.bat
+++ bin/abbozzaC.bat
@@ -7,5 +7,5 @@ set CP=..\lib\jSerialComm-2.4.2.jar;..\lib\rsyntaxtextarea.jar
 set CP=%CP%;..\lib\autocomplete.jar;..\lib\abbozza-calliope.jar
 set CP=%CP%;..\lib\commons-io-2.5.jar;..\lib\websockets.jar;..\lib\slf4j-api-1.7.25.jar
 
-javaw -cp %CP% de.uos.inf.did.abbozza.calliope.AbbozzaCalliopeC
+java -cp %CP% de.uos.inf.did.abbozza.calliope.AbbozzaCalliopeC
 
